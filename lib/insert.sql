76e425d4a50a575e69092af549f7d413b79b96ab
Brian Wangora <brianwangora@gmail.com>
2022-08-24 00:03:11 +0300

    tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 13, 'M', 'black', 'quiet', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'white', 'rowdy', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'yellow', 'aggressive', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'brown', 'quiet', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'green', 'active', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 19, 'M', 'red', 'loud', 'FALSE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'F', 'blue', 'quiet', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 17, 'F', 'black', 'unknown', 'TRUE');
